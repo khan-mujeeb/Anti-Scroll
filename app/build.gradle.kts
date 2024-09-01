cc621c7c076482a3687241d2b6d0f0ce80fea9fb
khan-mujeeb <mujeebkhan1831@gmail.com>
2024-09-01 20:09:57 +0530

    bottom navigation added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,13 +42,15 @@ android {
 dependencies {
 
     // 3rd party dependencies
-    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")     // chart
 
     // pre built dependencies
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
